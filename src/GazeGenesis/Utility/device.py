1919c4d431c037d6ddbd451c087c74c4a1c94f5f
S1N4M <sinamoghimi73@gmail.com>
2024-03-02 21:36:47 +0300

    device script to detect the hardware

---
+++ src/GazeGenesis/Utility/device.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import torch
+
+def get_device_name():
+    device = "cpu"
+    if torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        device = "mps"
+    elif torch.backends.cuda.is_available() and torch.backends.cuda.is_built():
+        device = "cuda"
+    
+    return device
