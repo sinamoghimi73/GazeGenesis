359b74ed8612ff7520fbf99cb456363b572f9cdf
S1N4M <sinamoghimi73@gmail.com>
2024-03-09 23:02:50 +0300

    modified to pass transforms to the LOADER initializer

--- src/GazeGenesis/ComputerVision/Datasets/CIFAR100.py
+++ src/GazeGenesis/ComputerVision/Datasets/CIFAR100.py
@@ -8,33 +8,22 @@ import torchvision.transforms as transforms
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-class CustomTransformer:
-    def __init__(self):
-        self.transform = transforms.Compose([
-            # transforms.ToPILImage(),
-            # transforms.RandomCrop((18,18)),
-            # transforms.RandomGrayscale(p = 0.2),
-            transforms.Resize((224,224)),
-            # transforms.RandomRotation(degrees = 30),
-            # transforms.ColorJitter(brightness = 0.5),
-            # transforms.RandomHorizontalFlip(p = 0.1),
-            transforms.ToTensor(),
-            # transforms.Normalize(mean = [0.1], std = [1.0]), # the array size here is variable with channel count. For mean=0 and std=1, the data remains unchanged and not normalized. We should find those values first manually.
-        ])
-
 class LOADER:
-    def __init__(self, validation_ratio = 0.3, train_batch_size = 64, test_batch_size = 64):
+    def __init__(self, validation_ratio = 0.3, train_batch_size = 64, test_batch_size = 64, transform = None):
         if not (validation_ratio < 1 and validation_ratio >= 0):
             raise RuntimeError('Validation ratio should be >= 0 and < 1.')
         
         print("Dataset: CIFAR100")
         self.dimension = (32,32)
-        self.custom_transformer = CustomTransformer()
+        if transform is not None:
+            self.custom_transformer = transform
+        else:
+            self.custom_transformer = transforms.ToTensor()
 
         current_directory = os.path.dirname(os.path.abspath(__file__))
-
-        self.train_dataset = datasets.CIFAR100(root = current_directory+"/CIFAR100", train = True, transform = self.custom_transformer.transform, download = True)
-        self.test_dataset = datasets.CIFAR100(root = current_directory+"/CIFAR100", train = False, transform = self.custom_transformer.transform, download = True)
+        
+        self.train_dataset = datasets.CIFAR100(root = current_directory+"/CIFAR100", train = True, transform = self.custom_transformer, download = True)
+        self.test_dataset = datasets.CIFAR100(root = current_directory+"/CIFAR100", train = False, transform = self.custom_transformer, download = True)
         
         total_train_count = len(self.train_dataset)
         self.train_dataset, self.validation_dataset = torch.utils.data.random_split(self.train_dataset, [int((1 - validation_ratio) * total_train_count), int(validation_ratio * total_train_count)])
--- src/GazeGenesis/ComputerVision/Datasets/MNIST.py
+++ src/GazeGenesis/ComputerVision/Datasets/MNIST.py
@@ -5,33 +5,22 @@ from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
-class CustomTransformer:
-    def __init__(self):
-        self.transform = transforms.Compose([
-            # transforms.ToPILImage(),
-            # transforms.RandomCrop((18,18)),
-            # transforms.RandomGrayscale(p = 0.2),
-            # transforms.Resize((28,28)),
-            # transforms.RandomRotation(degrees = 30),
-            # transforms.ColorJitter(brightness = 0.5),
-            # transforms.RandomHorizontalFlip(p = 0.1),
-            transforms.ToTensor(),
-            # transforms.Normalize(mean = [0.1], std = [1.0]), # the array size here is variable with channel count. For mean=0 and std=1, the data remains unchanged and not normalized. We should find those values first manually.
-        ])
-
 class LOADER:
-    def __init__(self, validation_ratio = 0.3, train_batch_size = 64, test_batch_size = 64):
+    def __init__(self, validation_ratio = 0.3, train_batch_size = 64, test_batch_size = 64, transform = None):
         if not (validation_ratio < 1 and validation_ratio >= 0):
             raise RuntimeError('Validation ratio should be >= 0 and < 1.')
         
         print("Dataset: MNIST")
         self.dimension = (28,28)
-        self.custom_transformer = CustomTransformer()
+        if transform is not None:
+            self.custom_transformer = transform
+        else:
+            self.custom_transformer = transforms.ToTensor()
 
         current_directory = os.path.dirname(os.path.abspath(__file__))
 
-        self.train_dataset = datasets.MNIST(root = current_directory, train = True, transform = self.custom_transformer.transform, download = True)
-        self.test_dataset = datasets.MNIST(root = current_directory, train = False, transform = self.custom_transformer.transform, download = True)
+        self.train_dataset = datasets.MNIST(root = current_directory, train = True, transform = self.custom_transformer, download = True)
+        self.test_dataset = datasets.MNIST(root = current_directory, train = False, transform = self.custom_transformer, download = True)
         
         total_train_count = len(self.train_dataset)
         self.train_dataset, self.validation_dataset = torch.utils.data.random_split(self.train_dataset, [int((1 - validation_ratio) * total_train_count), int(validation_ratio * total_train_count)])
