324993b0e4965397f125229c6d837b20562672b8
S1N4M <sinamoghimi73@gmail.com>
2024-03-09 23:03:36 +0300

    removed torch.backends.cuda.is_available() ->it had torch error

--- src/GazeGenesis/Utility/device.py
+++ src/GazeGenesis/Utility/device.py
@@ -6,7 +6,8 @@ def get_device_name():
     device = "cpu"
     if torch.backends.mps.is_available() and torch.backends.mps.is_built():
         device = "mps"
-    elif torch.backends.cuda.is_available() and torch.backends.cuda.is_built():
+    # elif torch.backends.cuda.is_available() and torch.backends.cuda.is_built():
+    elif torch.backends.cuda.is_built():
         device = "cuda"
     
     return device
