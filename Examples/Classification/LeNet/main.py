0402082d1e6c34baf222abe6254874644e5870c0
S1N4M <sinamoghimi73@gmail.com>
2024-03-09 12:02:40 +0300

    LeNet to use CIFAR10 -> in_channels=3

--- Examples/Classification/LeNet/main.py
+++ Examples/Classification/LeNet/main.py
@@ -2,7 +2,7 @@
 from GazeGenesis.ComputerVision.Classification.LeNet.user import User
 
 if __name__ == "__main__":
-    user = User(in_channels = 1, num_classes = 10, learning_rate = 1e-3, train_batch_size = 64, test_batch_size = 64)
+    user = User(in_channels = 3, num_classes = 10, learning_rate = 1e-3, train_batch_size = 64, test_batch_size = 64)
 
     user.train(epochs = 2)
     user.test()
