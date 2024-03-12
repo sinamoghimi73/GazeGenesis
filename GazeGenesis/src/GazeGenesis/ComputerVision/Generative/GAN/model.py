98bbf8c145260b43b987119f95793efb98bdd2eb
S1N4M <sinamoghimi73@gmail.com>
2024-03-12 23:33:16 +0300

    created a model with Discriminator and Generator

---
+++ GazeGenesis/src/GazeGenesis/ComputerVision/Generative/GAN/model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import torch
+import torch.nn as nn
+from GazeGenesis.Utility.device import get_device_name
+
+
+class Discriminator(nn.Module):
+    def __init__(self, input_dim):
+        super(Discriminator, self).__init__()
+        print("MODEL: Discriminator")
+
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.LeakyReLU(0.1),
+            nn.Linear(128, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+    
+
+
+class Generator(nn.Module):
+    def __init__(self, noise_dim, output_dim):
+        super(Generator, self).__init__()
+        print("MODEL: Generator")
+
+        self.layers = nn.Sequential(
+            nn.Linear(noise_dim, 256),
+            nn.LeakyReLU(0.1),
+            nn.Linear(128, output_dim),
+            nn.Softsign()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
