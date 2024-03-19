de022893364a126aa6895a582bc11d5943e61f5d
S1N4M <sinamoghimi73@gmail.com>
2024-03-19 18:19:58 +0300

    to uncomment the transforms.Normalize -> it is a must to exist

--- Examples/Generative/GAN/main.py
+++ Examples/Generative/GAN/main.py
@@ -7,11 +7,11 @@ import math, os
 if __name__ == "__main__":
     transform = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.1307),(0.3081))
+            # transforms.Normalize((0.1307),(0.3081))
         ])
-    loader = LOADER(validation_ratio=0.3, train_batch_size = 16, test_batch_size = 64, transform = transform)
+    loader = LOADER(validation_ratio=0.01, train_batch_size = 32, test_batch_size = 64, transform = transform)
 
     current_adress = os.path.dirname(__file__)
     user = User(input_dim = math.prod(loader.dimension), noise_dim = 64, learning_rate = 3e-4, loader=loader, summary_writer_address = current_adress + "/runs/GAN/")
 
-    user.train(epochs = 10)
+    user.train(epochs = 50)
