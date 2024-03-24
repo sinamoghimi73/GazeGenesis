b41b38f87464c8305cd6c400f028e62ac16f4575
S1N4M <sinamoghimi73@gmail.com>
2024-03-24 21:20:28 +0300

    update dcgan model

--- GazeGenesis/src/GazeGenesis/ComputerVision/Generative/DCGAN/model.py
+++ GazeGenesis/src/GazeGenesis/ComputerVision/Generative/DCGAN/model.py
@@ -17,8 +17,10 @@ class Discriminator(nn.Module):
             self.block_(in_channels = features_d * 1, out_channels = features_d  * 2, kernel_size = 4, stride = 2, padding = 1), # 16*16
             self.block_(in_channels = features_d * 2, out_channels = features_d  * 4, kernel_size = 4, stride = 2, padding = 1), # 8*8
             self.block_(in_channels = features_d * 4, out_channels = features_d  * 8, kernel_size = 4, stride = 2, padding = 1), # 4*4
-            nn.Conv2d(features_d * 8, 1, kernel_size = 4, stride = 2, padding = 1) # 1*1
+            nn.Conv2d(features_d * 8, 1, kernel_size = 4, stride = 2, padding = 0) # 1*1
         )
+
+        self.initialize_weights(self.layers)
 
         
     def block_(self, in_channels, out_channels, kernel_size, stride, padding):
@@ -30,6 +32,12 @@ class Discriminator(nn.Module):
     
     def forward(self, x):
         return self.layers(x)
+    
+    # we need to initialize the weights with mean=0 and std=0.02
+    def initialize_weights(self, model):
+        for m in  model.modules():
+            if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+                nn.init.normal_(m.weight.data, 0.0, 0.02)
     
 
 
@@ -48,6 +56,8 @@ class Generator(nn.Module):
             nn.Tanh() # [-1, 1]
         )
 
+        self.initialize_weights(self.layers)
+
     def block_(self, in_channels, out_channels, kernel_size, stride, padding):
             return nn.Sequential(
                 nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias = False),
@@ -59,19 +69,25 @@ class Generator(nn.Module):
         return self.layers(x)
 
 
-# we need to initialize the weights with mean=0 and std=0.02
-def initialize_weights(model):
-     for m in  model.modules():
-          if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
-               nn.init.normal_(m.weight.data, 0.0, 0.02)
+    # we need to initialize the weights with mean=0 and std=0.02
+    def initialize_weights(self, model):
+        for m in  model.modules():
+            if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+                nn.init.normal_(m.weight.data, 0.0, 0.02)
 
-def test():
-     N, in_channels, H, W = 8, 3, 64, 64
-     noise_dim = 100
-     x = torch.randn((N, in_channels, H, W))
-     dicriminator = Discriminator(in_channels=in_channels, features_d = 8 )
-     initialize_weights(dicriminator)
+# def test():
+#     N, in_channels, H, W = 8, 3, 64, 64
+#     noise_dim = 100
+#     x = torch.randn((N, in_channels, H, W))
+#     dicriminator = Discriminator(in_channels=in_channels, features_d = 8 )
+#     # dicriminator.initialize_weights(dicriminator)
+#     assert dicriminator(x).shape == (N, 1, 1, 1)
 
-     assert dicriminator(x).shape == (N, 1, 1, 1)
+#     generator = Generator(noise_dim = noise_dim, out_channels = in_channels, features_g = 8)
+#     # generator.initialize_weights(generator)
+#     z = torch.randn((N, noise_dim, 1, 1))
+#     assert generator(z).shape == (N, in_channels, H, W)
+#     print("Success")
 
-test()
+
+# test()
